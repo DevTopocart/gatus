7403d89957719d19bdc7d110e0b0fcc44ae70148
Victor Marotta <victordossantosmarotta@gmail.com>
2024-11-01 11:17:23 -0300

    feat: adiciona endpoint para obtencao do response-time medio das APIs

--- api/raw.go
+++ api/raw.go
@@ -41,3 +41,45 @@ func UptimeRaw(c *fiber.Ctx) error {
 	c.Set("Expires", "0")
 	return c.Status(200).Send([]byte(fmt.Sprintf("%f", uptime)))
 }
+
+// ResponseTimeRaw retorna o tempo de resposta médio em texto simples
+func ResponseTimeRaw(c *fiber.Ctx) error {
+    duration := c.Params("duration")
+    var from time.Time
+
+    // Define o intervalo de tempo com base na duração fornecida
+    switch duration {
+    case "30d":
+        from = time.Now().Add(-30 * 24 * time.Hour)
+    case "7d":
+        from = time.Now().Add(-7 * 24 * time.Hour)
+    case "24h":
+        from = time.Now().Add(-24 * time.Hour)
+    case "1h":
+        from = time.Now().Add(-2 * time.Hour) // Ajuste necessário para métricas armazenadas por hora
+    default:
+        return c.Status(400).SendString("Durations supported: 30d, 7d, 24h, 1h")
+    }
+
+    key := c.Params("key")
+
+    // Obtém o tempo de resposta médio a partir do armazenamento
+    averageResponseTime, err := store.Get().GetAverageResponseTimeByKey(key, from, time.Now())
+    if err != nil {
+        if errors.Is(err, common.ErrEndpointNotFound) {
+            return c.Status(404).SendString(err.Error())
+        } else if errors.Is(err, common.ErrInvalidTimeRange) {
+            return c.Status(400).SendString(err.Error())
+        }
+        return c.Status(500).SendString(err.Error())
+    }
+
+    // Define os cabeçalhos de resposta
+    c.Set("Content-Type", "text/plain")
+    c.Set("Cache-Control", "no-cache, no-store, must-revalidate")
+    c.Set("Expires", "0")
+
+    // Retorna o tempo de resposta médio como texto simples
+    return c.Status(200).Send([]byte(fmt.Sprintf("%d", averageResponseTime)))
+
+}
